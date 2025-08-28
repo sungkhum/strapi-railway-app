75d03f93e937775a128228e5ca096041fb83ef11
Nathan <sungkhum@gmail.com>
2025-08-27 20:07:05 -0700

    update

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -3,14 +3,15 @@ import type { Schema, Struct } from '@strapi/strapi';
 export interface AudioBooksChapter extends Struct.ComponentSchema {
   collectionName: 'components_audio_books_chapters';
   info: {
+    description: '';
     displayName: 'Chapter';
     icon: 'file';
   };
   attributes: {
     audio_file: Schema.Attribute.Media<'audios'>;
     audio_url: Schema.Attribute.String;
-    chapter_title: Schema.Attribute.String;
     duration: Schema.Attribute.String;
+    title: Schema.Attribute.String;
   };
 }
 
