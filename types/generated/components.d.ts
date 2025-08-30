6dcd7f0e86e6a9e315de550232e4375d2c5a2448
vishwajeet <vishwajeeet@aol.com>
2025-08-30 16:28:55 +0700

    add chapter component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,23 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface AudioBooksChapters extends Struct.ComponentSchema {
+  collectionName: 'components_audio_books_chapters';
+  info: {
+    displayName: 'Chapter';
+    icon: 'bulletList';
+  };
+  attributes: {
+    audio_file: Schema.Attribute.Media<'audios'>;
+    audio_url: Schema.Attribute.String;
+    duration: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'audio-books.chapters': AudioBooksChapters;
+    }
+  }
+}
